a3180f510ac3fda5d0de9fe58ca672f0f8913750
NgoDinhHoangHa <hoanghadn0123@gmail.com>
2023-11-22 21:53:55 +0700

    version - demo

--- web_hospitall/src/components/user/components/layout/Header.vue
+++ web_hospitall/src/components/user/components/layout/Header.vue
@@ -1,80 +1,74 @@
 <template>
-    <header id="header" class="header">
-        <div class="wrapper header__content">
-            <div class="header__icon">
-                <span @click="isActive = !isActive" class="bx bx-menu"></span>
-            </div>
+    <header id="header" class="header" style="height: 80px; text-align: center;  ">
+        <div class="wrapper header__content" style="justify-content: center; margin-top: 60px;">
             <div class="header__logo">
                 <router-link :to="{ name: 'User_Home' }">
-                    <img src="@/assets/img/banner/doctor.png" alt="" srcset="">
+                    <img src="https://bookingcare.vn/assets/icon/bookingcare-2020.svg" alt="" srcset="" style="margin-left: 40px;">
                 </router-link>
             </div>
-            <ul class="header__list">
-                <li class="hide-show">
-                    <p>Chuyên khoa</p>
-                    <p>Tìm bác sỹ theo chuyên khoa</p>
-                </li>
-                <li class="hide-show">
-                    <p>Chuyên khoa</p>
-                    <p>Tìm bác sỹ theo chuyên khoa</p>
-                </li>
-                <li class="hide-show">
-                    <p>Chuyên khoa</p>
-                    <p>Tìm bác sỹ theo chuyên khoa</p>
-                </li>
-                <li class="hide-show">
-                    <p>Chuyên khoa</p>
-                    <p>Tìm bác sỹ theo chuyên khoa</p>
-                </li>
-            </ul>
-            <div class="header__support" @click="this.$router.push({ name: (user ? 'Profile' : 'Login') })">
+            <div id="menu">
+                <ul>
+                    <li>
+                        <router-link @click="isActive = !isActive" :to="{
+                            name: 'User_Home'
+                        }">
+                            Trang chủ
+                        </router-link>
+                    </li>
+                    <li v-if="user">
+                        <router-link @click="isActive = !isActive" :to="{
+                            name: 'Profile',
+                            params: {
+                                id: this.user.id
+                            }
+                        }">
+                            Cập nhật thông tin cá nhân
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link @click="isActive = !isActive" :to="{
+                            name: 'DoctorList'
+                        }">
+                            Bác sĩ
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link @click="isActive = !isActive" :to="{
+                            name: 'Blogs'
+                        }">
+                            Bài viết
+                        </router-link>
+                    </li>
+                </ul>
+                </div>
+            <div class="header__support" style="margin-left: 60px;" @click="this.$router.push({ name: (user ? 'Profile' : 'Login') })">
                 <i class='bx bxs-user' style='color:#318c9c'></i>
                 <span>{{ user ? user.fullname : "Đăng nhập" }}</span>
             </div>
-            <div @click="logout()" v-if="user != null" class="header__support">
+            <div style="margin-left: 20px;" @click="logout()" v-if="user != null" class="header__support">
                 <i class='bx bx-exit' style='color:#318c9c'></i>
                 <span></span>
             </div>
         </div>
-        <div class="header__menu" :class="isActive ? 'active' : ''">
-            <ul>
-                <li>
-                    <router-link @click="isActive = !isActive" :to="{
-                        name: 'User_Home'
-                    }">
-                        Trang chủ
-                    </router-link>
-                </li>
-                <li v-if="user">
-                    <router-link @click="isActive = !isActive" :to="{
-                        name: 'Profile',
-                        params: {
-                            id: this.user.id
-                        }
-                    }">
-                        Cập nhật thông tin cá nhân
-                    </router-link>
-                </li>
-                <li>
-                    <router-link @click="isActive = !isActive" :to="{
-                        name: 'DoctorList'
-                    }">
-                        Bác sĩ
-                    </router-link>
-                </li>
-                <li>
-                    <router-link @click="isActive = !isActive" :to="{
-                        name: 'Blogs'
-                    }">
-                        Bài viết
-                    </router-link>
-                </li>
-            </ul>
-        </div>
-        <div @click="isActive = false" class="header__menu--overlay" :class="isActive ? 'active' : ''">
-        </div>
     </header>
 </template>
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    #menu ul {
+        list-style-type: none;
+        text-align: center;
+        width: 100%;
+    }
+    #menu li {
+        color: #f1f1f1;
+        display: inline-block;
+        margin-left: 50px;
+      }
+    
+</style>
 <script>
 import { mapState, mapMutations } from 'vuex'
 export default {
